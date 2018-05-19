6db724cd566bba0e437b03399822a194130bd581
fabs <faebeee@gmail.com>
2018-05-19 12:53:02 +0200

    Avoid parralel executions

--- src/cli.ts
+++ src/cli.ts
@@ -1,21 +1,20 @@
 #!/usr/bin/env node
 
 import {Command} from 'commander';
+import run from './index';
 
 const program = require('commander');
-import run from './index';
 
 const {version} = require('../package.json');
 
 program
     .version(version);
 
-
 program
-    .command('audit <url>', {isDefault: true})
+    .command('audit <url> <path>', {isDefault: true})
     .description('Run report with configuration')
-    .action(async (url: string, command: Command) => {
-        await run(url);
+    .action(async (url: string, path: string, command: Command) => {
+        await run(url, path);
         return;
     });
 
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,24 @@
 import Dreihouse from "@dreipol/lighthouse-runner/dist/Dreihouse";
 
 const CONFIG = require('@dreipol/lighthouse-config/config/base/desktop.js');
-var chokidar = require('chokidar');
+const chokidar = require('chokidar');
+
+CONFIG.chromeFlags = ['--headless'];
 
 const dreihouse = new Dreihouse(CONFIG, ['cli']);
+let isAuditing = false;
 
-export default async function run(url: string) {
+export default async function run(url: string, path: string) {
     await dreihouse.startChrome(url);
 
-    chokidar.watch('./src', {ignored: /(^|[\/\\])\../})
+    chokidar.watch(path, {ignored: /(^|[\/\\])\../})
         .on('change', async () => {
+            if (isAuditing) {
+                return;
+            }
+
+            isAuditing = true;
             await dreihouse.audit(url);
+            isAuditing = false;
         });
 }
